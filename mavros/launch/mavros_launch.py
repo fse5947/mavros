3360ffbfcc40c38cc7f42b1de7b4a887f481ed28
Fares <fares.eltin@mail.mcgill.ca>
2022-12-03 21:30:56 -0500

    Added launch file

---
+++ mavros/launch/mavros_launch.py
@@ -0,0 +1,19 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    mavros_params_file = os.path.join(
+        get_package_share_directory('mavros'), 'config',
+        'mavros_params.yaml')
+
+    return LaunchDescription([
+        Node(package='mavros',
+             executable='mavros_node',
+             output='screen',
+             emulate_tty=True,
+             parameters=[mavros_params_file]),
+    ])
