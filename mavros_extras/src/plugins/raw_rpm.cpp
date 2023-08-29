182ca1a1b8503fd80dd5fcd999d20b956e736f94
Fares <fares.eltin@mail.mcgill.ca>
2023-08-29 19:59:24 -0400

    Added raw_rpm mavros plugin

---
+++ mavros_extras/src/plugins/raw_rpm.cpp
@@ -0,0 +1,60 @@
+
+#include "mavros/mavros_uas.hpp"
+#include "mavros/plugin.hpp"
+#include "mavros/plugin_filter.hpp"
+
+#include "mavros_msgs/msg/raw_rpm.hpp"
+
+namespace mavros
+{
+namespace extra_plugins
+{
+using namespace std::placeholders;      // NOLINT
+
+/**
+ * @brief RAW RPM plugin.
+ * @plugin raw_rpm
+ */
+class RawRPMPlugin : public plugin::Plugin
+{
+public:
+  explicit RawRPMPlugin(plugin::UASPtr uas_)
+  : Plugin(uas_, "raw_rpm")
+  {
+    rpm_pub = node->create_publisher<mavros_msgs::msg::RawRPM>("raw_rpm", 10);
+  }
+
+  Subscriptions get_subscriptions() override
+  {
+    return {
+      make_handler(&RawRPMPlugin::handle_raw_rpm),
+    };
+  }
+
+private:
+  rclcpp::Publisher<mavros_msgs::msg::RawRPM>::SharedPtr rpm_pub;
+
+  void handle_raw_rpm(
+    const mavlink::mavlink_message_t * msg [[maybe_unused]],
+    mavlink::common::msg::RAW_RPM & raw_rpm,
+    plugin::filter::SystemAndOk filter [[maybe_unused]])
+  {
+    auto vmsg = mavros_msgs::msg::RawRPM();
+
+    vmsg.header.stamp = node->now();
+    vmsg.frequency = raw_rpm.frequency;
+    // vmsg.airspeed = vfr_hud.airspeed;
+    // vmsg.groundspeed = vfr_hud.groundspeed;
+    // vmsg.heading = vfr_hud.heading;
+    // vmsg.throttle = vfr_hud.throttle / 100.0;   // comes in 0..100 range
+    // vmsg.altitude = vfr_hud.alt;
+    // vmsg.climb = vfr_hud.climb;
+
+    rpm_pub->publish(vmsg);
+  }
+};
+}       // namespace extra_plugins
+}       // namespace mavros
+
+#include <mavros/mavros_plugin_register_macro.hpp>  // NOLINT
+MAVROS_PLUGIN_REGISTER(mavros::extra_plugins::RawRPMPlugin)
